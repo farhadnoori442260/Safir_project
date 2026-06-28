fb4d047e51bef2952f110ddd5947f4a991c5ced8
farhadnoori442260 <farhadnoori552@gmail.com>
2026-06-29 02:02:24 +0330

    Initialize driver info and trip status variables
    Added variables for driver information and trip status.

---
+++ safir_passengers/lib/globle/trip_var.dart
@@ -0,0 +1,9 @@
+String nameDriver = '';
+String photoDriver = '';
+String phoneNumberDriver = '';
+int requestTimeoutDriver = 40; // مدت زمان انتظار برای قبول سفر توسط راننده (۴۰ ثانیه)
+String status = '';
+String carDetailsDriver = '';
+
+// بومی‌سازی متن پیش‌فرض وضعیت سفر برای مسافر
+String tripStatusDisplay = 'راننده در حال آمدن است...';
