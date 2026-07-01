0c56ed2a172398fc4ddaf0ac3247596f8a20d1e7
farhadnoori442260 <farhadnoori552@gmail.com>
2026-07-01 21:34:33 +0330

    Add OnlineNearbyDrivers model class

---
+++ safir_passengers/lib/models/online_nearby_drivers.dart
@@ -0,0 +1,11 @@
+class OnlineNearbyDrivers {
+  String? uidDriver;  // آی‌دی منحصر به فرد راننده در فایربیس
+  double? latDriver;  // موقعیت عرض جغرافیایی راننده
+  double? lngDriver;  // موقعیت طول جغرافیایی راننده
+
+  OnlineNearbyDrivers({
+    this.uidDriver,
+    this.latDriver,
+    this.lngDriver,
+  });
+}
