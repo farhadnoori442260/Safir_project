954efec8703dc67391704288206ec12010887103
farhadnoori442260 <farhadnoori552@gmail.com>
2026-07-05 01:19:23 +0330

    Add primary and secondary color constants
    Define primary and secondary colors for the app.

---
+++ safir_drivers/lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// تغییر رنگ اصلی به سبز زمردی تیره سفیر (جایگزین آبی قدیمی اوبر)
+const primaryColor = Color(0xFF145A41);
+
+// تغییر رنگ ثانویه به سفید مایل به خاکستری روشن برای پس‌زمینه‌ها یا عناصر کم‌اهمیت‌تر
+const secondaryColor = Color(0xFFF8F9FA); 
