8241c08ad681c430eedb371dc3e6958f46abf329
farhadnoori442260 <farhadnoori552@gmail.com>
2026-07-05 01:48:08 +0330

    Create MainActivity for Safir Drivers Flutter app
    Add MainActivity class for Flutter integration in Safir Drivers package

---
+++ safir_drivers/android/app/src/main/kotlin/com/example/safir_drivers/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.safir_drivers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // این بخش بدنه اسکلت اصلی فلاتر را برای پکیج جدید سفیر حفظ می‌کند
+}
