2bde0d9027a2f767ad5692a6a686bc64142ec6fd
farhadnoori442260 <farhadnoori552@gmail.com>
2026-07-08 00:54:42 +0330

    Enhance image cropping with UI settings
    Added UI settings for cropping images in the ImagePickerService.

--- safir_drivers/lib/methods/image_picker_service.dart
+++ safir_drivers/lib/methods/image_picker_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -8,11 +9,28 @@ class ImagePickerService {
   }) async {
     XFile? pickImage = await ImagePicker().pickImage(source: imageSource);
     if (pickImage == null) return null;
+    
     CroppedFile? croppedFile = await ImageCropper().cropImage(
         sourcePath: pickImage.path,
         aspectRatio: cropAspectRatio,
         compressQuality: 90,
-        compressFormat: ImageCompressFormat.jpg);
+        compressFormat: ImageCompressFormat.jpg,
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: 'ویرایش عکس پروفایل', // عنوان تولبار کراپ
+            toolbarColor: const Color(0xFF145A41), // رنگ سبز اختصاصی سفیر
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.square,
+            lockAspectRatio: true,
+          ),
+          IOSUiSettings(
+            title: 'ویرایش عکس پروفایل',
+            doneButtonTitle: 'تایید',
+            cancelButtonTitle: 'انصراف',
+          ),
+        ],
+    );
+    
     if (croppedFile == null) return null;
     return XFile(croppedFile.path);
   }
